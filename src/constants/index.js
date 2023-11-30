051137d75fc691b8ad9881b4e83ca4a97f9f9fb5
Memory&Diamond <51429928+memory-and-diamond@users.noreply.github.com>
2023-11-30 13:50:45 +0100

    index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -25,56 +25,56 @@ export const services = [
     {
         color: 'bg-[#A0CCF5]',
         title: 'Photographers',
-        img: '/public/Images/photographer.jpg',
+        img: 'public/Images/photographer.jpg',
         margin: 'mt-10'
     },
     {
         color: 'bg-yellow-400 ',
         title: 'Halls',
-        img: '/public/Images/hall.jpg',
+        img: 'public/Images/hall.jpg',
         margin: 'mt-0'
     },
     {
         color: 'bg-fuchsia-500',
         title: 'Cakes',
-        img: '/public/Images/cake.jpg',
+        img: 'public/Images/cake.jpg',
         margin: 'mt-6'
     },
     {
         color: 'bg-lime-300',
         title: 'Dress',
-        img: '/public/Images/dress.jpg',
+        img: 'public/Images/dress.jpg',
         margin: 'mt-5'
     },
     {
         color: 'bg-purple-400',
         title: 'DJs',
-        img: '/public/Images/dj.jpg',
+        img: 'public/Images/dj.jpg',
         margin: '-mt-5'
     },
     {
         color: 'bg-red-400 ',
         title: 'Package',
-        img: '/public/Images/package.jpg',
+        img: 'public/Images/package.jpg',
         margin: 'mt-1'
     },
 ]
 
 export const strengths = [
     {
-        imageUrl: "/public/Icons/Satisfaction.png",
+        imageUrl: "public/Icons/Satisfaction.png",
         title: "Clients Satisfaction",
         content:
             "Our Commitment to Clients: We are passionate about creating unforgettable wedding experiences for our clients. We collaborate closely with them to understand their desires and produce projections that perfectly capture their unique love story.",
     },
     {
-        imageUrl: "/public/Icons/Experience.png",
+        imageUrl: "public/Icons/Experience.png",
         title: "Experiences",
         content:
             "Our Wedding Expertise: We boast a team of wedding experts who possess in-depth knowledge of wedding trends and traditions. We can assist you in crafting a truly unique and memorable wedding experience.",
     },
     {
-        imageUrl: "/public/Icons/Ai.png",
+        imageUrl: "public/Icons/Ai.png",
         title: "Technologies",
         content:
             "Our Passion for Innovation: We continuously seek new avenues to utilize technology in creating even more immersive and emotionally resonant wedding experiences",
